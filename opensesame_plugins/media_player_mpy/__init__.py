b80da6b30425a1bd7dded164c01e66676d456eba
smathot <s.mathot@cogsci.nl>
2023-03-21 20:13:34 +0100

    Remove opensesame-core as dependency - To avoid package-name conflicts - Add package alias - Bump to 0.2.1

--- opensesame_plugins/media_player_mpy/__init__.py
+++ opensesame_plugins/media_player_mpy/__init__.py
@@ -1,4 +1,5 @@
 """OpenSesame core extensions"""
 
 # The name of the packages to check for updates on conda and pip
-packages = ['opensesame-plugin-media_player_mpy']
+packages = ['opensesame-plugin-media_player_mpy',
+            'opensesame-plugin-media-player-mpy']
