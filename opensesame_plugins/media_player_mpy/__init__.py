4334ec3696cd2f5df013b5ae27ba959c8cfc30eb
smathot <s.mathot@cogsci.nl>
2025-04-07 11:42:47 +0200

    Include mediadecoder in packages to trigger updates

--- opensesame_plugins/media_player_mpy/__init__.py
+++ opensesame_plugins/media_player_mpy/__init__.py
@@ -2,4 +2,5 @@
 
 # The name of the packages to check for updates on conda and pip
 packages = ['opensesame-plugin-media_player_mpy',
-            'opensesame-plugin-media-player-mpy']
+            'opensesame-plugin-media-player-mpy',
+            'mediadecoder']
